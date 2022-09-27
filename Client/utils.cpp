0e50446304813487d20e8c5ee27b464d1015299e
Efrain.mesia <efrain.mesia@gmail.com>
2022-09-27 18:57:18 +0300

    Added base64 handler and string utils

--- Client/utils.cpp
+++ Client/utils.cpp
@@ -1,12 +1,35 @@
 #include "utils.h"
 
-uint32_t doCrc(uint8_t* bytes, unsigned int bytesLength)
-{
-    uint32_t crc = crc32(0L, Z_NULL, 0);
-    for (unsigned int i = 0; i < bytesLength; ++i)
-    {
-        crc = crc32(crc, bytes + i, 1);
+uint8_t countDelim(std::string str, char delim) {
+    uint8_t count = 0;
+    for (char& c : str) {
+        if (c == delim)
+            count++;
     }
-    LOG(crc);
-    return crc;
+    return count;
+}
+
+
+std::string encodeBase64(const std::string& str)
+{
+    std::string encoded;
+    CryptoPP::StringSource ss(str, true,
+        new CryptoPP::Base64Encoder(
+            new CryptoPP::StringSink(encoded)
+        ) // Base64Encoder
+    ); // StringSource
+
+    return encoded;
+}
+
+std::string decodeBase64(const std::string& str)
+{
+    std::string decoded;
+    CryptoPP::StringSource ss(str, true,
+        new CryptoPP::Base64Decoder(
+            new CryptoPP::StringSink(decoded)
+        ) // Base64Decoder
+    ); // StringSource
+
+    return decoded;
 }
--- Client/utils.h
+++ Client/utils.h
@@ -3,9 +3,43 @@
 #include <filesystem>
 #include <iostream>
 #include <zlib.h>
+#include "Base64Wrapper.h"
 
 
-#define LOG_ERROR(x) (std::cerr <<"[+] ERROR:" << x << std::endl)
+
+#define LOG_ERROR(x) (std::cerr <<"[+] ERROR: " << x << std::endl)
 #define LOG(x) (std::cout << "[+] INFO: "<< x << std::endl)
 
-uint32_t doCrc(uint8_t* bytes, unsigned int bytesLength);
+uint8_t countDelim(std::string, char);
+std::string encodeBase64(const std::string&);
+std::string decodeBase64(const std::string&);
+
+// trim from start (in place)
+static inline void ltrim(std::string& s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+        }));
+}
+
+// trim from end (in place)
+static inline void rtrim(std::string& s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+        }).base(), s.end());
+}
+
+// trim from both ends (in place)
+static inline void trim(std::string& s) {
+    ltrim(s);
+    rtrim(s);
+}
+
+static bool isAlNum(std::string& s) {
+    //check if its only alphanumeric
+    for (auto ch : s) {
+        if (!std::isalnum(ch)) {
+            return false;
+        }
+    }
+    return true;
+}
