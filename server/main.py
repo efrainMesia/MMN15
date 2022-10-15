99a3c509fcb327f1b336ffa547d0b1fe0eba6808
Efrain.mesia <efrain.mesia@gmail.com>
2022-10-15 11:56:22 +0300

    users only server function

--- server/main.py
+++ server/main.py
@@ -1,12 +1,11 @@
 import server
 
 if __name__ == '__main__':
-    #PORT_INFO = "port.info"
-    #port = utils.parsePort(PORT_INFO)
+
     port = 7777
     #if port is None:
     #    utils.stopServer(f"Failed to parse integer port from '{PORT_INFO}'!")
-    svr = server.Server('', port)  # don't care about host.
+    svr = server.Server()  # don't care about host.
     svr.start()
     #if not svr.start():
     #    utils.stopServer(f"Server start exception: {svr.lastErr}")
